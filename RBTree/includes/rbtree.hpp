fde01d57d7c91812b49729307b7765f118e9c7f6
Mirage <andrej_malinin@mail.ru>
2022-03-11 18:34:49 +0300

    added rbtree constructor and struct node

--- RBTree/includes/rbtree.hpp
+++ RBTree/includes/rbtree.hpp
@@ -1,9 +1,34 @@
 #ifndef RBTREE_INCLUDES_RBTREE
 #define RBTREE_INCLUDES_RBTREE
 
-template<typename T>
-class RBTree {
+namespace Trees {
+	enum {BLACK, RED};
 
-};
+	template<typename T>
+	struct node {
+		node(int col = BLACK);
+		~node();
+		int color;
+		size_t num_of_less;
+		size_t num_of_greater;
+		node<T>* left;
+		node<T>* right;
+		node<T>* parent;
+		T* data;
+	};
+
+	template<typename T>
+	class RBTree {
+	public:
+		RBTree();
+		//RBTree(const RBTree& another);
+		//RBTree(RBTree&& another);
+		~RBTree();
+
+	private:
+		node<T>* nil_;
+		node<T>* head_;
+	};
+}
 
 #endif // RBTREE_INCLUDES_RBTREE
--- RBTree/includes/rbtree_impl.hpp
+++ RBTree/includes/rbtree_impl.hpp
@@ -3,6 +3,40 @@
 
 #include "rbtree.hpp"
 
+namespace Trees {
 
+	template<typename T>
+	node<T>::node(int col) : color(col), num_of_less(0), num_of_greater(0), left(nullptr), right(nullptr), parent(nullptr), data(nullptr) {}
+
+	template<typename T>
+	node<T>::~node() {
+		delete left;
+		delete right;
+		delete parent;
+		delete[] data;
+	}
+
+	template<typename T>
+	RBTree<T>::RBTree() : nil_(new node<T>), head_(nil_) {}
+
+	template<typename T>
+	RBTree<T>::~RBTree() {
+		node<T> cur = head_;
+		while (head_ != nullptr && head_ != nil_) {
+			while (cur->left != nullptr && cur->left != nil_) {
+				cur = cur->left;
+			}
+			while (cur->right != nullptr && cur->right != nil_) {
+				cur = cur->right;
+			}
+			if (cur != head_) {
+				cur = cur->parent;
+				
+			}
+
+		}
+	}
+
+}
 
 #endif // RBTREE_INCLUDES_RBTREE_IMPL
