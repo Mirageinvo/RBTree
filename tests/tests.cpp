e183073aa1be2c0976b34fc6f6ada4d3170ddcac
Mirage <andrej_malinin@mail.ru>
2022-03-15 00:02:59 +0300

    started adding tests

---
+++ tests/tests.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "rbtree_impl.hpp"
+
+TEST(RBTree, CopyConstructor) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
