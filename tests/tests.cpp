8ea4a1c9864de015fb64105b00fa4355ae4aa6c5
Mirage <andrej_malinin@mail.ru>
2022-03-15 01:53:02 +0300

    mode tests added

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,11 +1,66 @@
 #include "gtest/gtest.h"
 #include "rbtree_impl.hpp"
 
-TEST(RBTree, CopyConstructor) {
-    
+const size_t kNum = 1e5;
+
+TEST(RBTree, InsertNumOfLessMthStatisticTest) {
+    Trees::RBTree<size_t> tmp_1;
+    for (size_t i = 1; i <= kNum; ++i) {
+        tmp_1.insert(i);
+        ASSERT_TRUE(tmp_1.mth_statistic(i) == i);
+        ASSERT_TRUE(tmp_1.num_of_less(i + 1) == i);
+    }
+    Trees::RBTree<size_t> tmp_2;
+    for (size_t i = kNum; i >= 1; --i) {
+        tmp_2.insert(i);
+        ASSERT_TRUE(tmp_2.mth_statistic(1) == i);
+        ASSERT_TRUE(tmp_2.num_of_less(kNum + 1) == kNum - i + 1);
+    }
 }
 
+TEST(RBTree, CopyConstructorTest) {
+    auto tmp_1 = new Trees::RBTree<size_t>;
+    for (size_t i = 1; i <= kNum; ++i) {
+        tmp_1->insert(i);
+        ASSERT_TRUE(tmp_1->mth_statistic(i) == i);
+        ASSERT_TRUE(tmp_1->num_of_less(i + 1) == i);
+    }
+    Trees::RBTree<size_t> tmp_2(*tmp_1);
+    delete tmp_1;
+    for (size_t i = 1; i <= kNum; ++i) {
+        ASSERT_TRUE(tmp_2.mth_statistic(i) == i);
+        ASSERT_TRUE(tmp_2.num_of_less(i + 1) == i);
+    }
+}
 
+TEST(RBTree, MoveConstructorTest) {
+    Trees::RBTree<size_t> tmp_1;
+    for (size_t i = 1; i <= kNum; ++i) {
+        tmp_1.insert(i);
+        ASSERT_TRUE(tmp_1.mth_statistic(i) == i);
+        ASSERT_TRUE(tmp_1.num_of_less(i + 1) == i);
+    }
+    Trees::RBTree<size_t> tmp_2(std::move(tmp_1));
+    for (size_t i = 1; i <= kNum; ++i) {
+        ASSERT_TRUE(tmp_2.mth_statistic(i) == i);
+        ASSERT_TRUE(tmp_2.num_of_less(i + 1) == i);
+    }
+}
+
+TEST(RBTree, MoveAssignOperatorTest) {
+    Trees::RBTree<size_t> tmp_1;
+    for (size_t i = 1; i <= kNum; ++i) {
+        tmp_1.insert(i);
+        ASSERT_TRUE(tmp_1.mth_statistic(i) == i);
+        ASSERT_TRUE(tmp_1.num_of_less(i + 1) == i);
+    }
+    Trees::RBTree<size_t> tmp_2;
+    tmp_2 = std::move(tmp_1);
+    for (size_t i = 1; i <= kNum; ++i) {
+        ASSERT_TRUE(tmp_2.mth_statistic(i) == i);
+        ASSERT_TRUE(tmp_2.num_of_less(i + 1) == i);
+    }
+}
 
 
 
