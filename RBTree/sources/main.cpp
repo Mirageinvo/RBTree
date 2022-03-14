9675c1051d98353ba5747321bf363298ce8ea9bc
Mirage <andrej_malinin@mail.ru>
2022-03-14 20:57:22 +0300

    added some functions

--- RBTree/sources/main.cpp
+++ RBTree/sources/main.cpp
@@ -2,21 +2,22 @@
 #include "rbtree_impl.hpp"
 
 int main() {
-    char sign;
+    std::string sign;
     int num;
     Trees::RBTree<int> tree;
     std::cin >> sign;
     while (!std::cin.eof()) {
-        if (sign == 'k') {
+        if (sign.size() == 1 && sign == "k") {
             std::cin >> num;
             tree.insert(num);
         }
-        /*else if (sign == 'm') {
-
-        }
-        else if (sign == 'n') {
-
+        /*else if (sign.size() == 1 && sign == "m") {
+            std::cin >> num;
         }*/
+        else if (sign.size() == 1 && sign == "n") {
+            std::cin >> num;
+            std::cout << tree.num_of_less(num) << std::endl;
+        }
         else {
             //std::cout << "Unknown command" << std::endl;
             return 0;
